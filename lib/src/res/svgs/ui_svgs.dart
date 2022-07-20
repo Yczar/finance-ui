2940bf7b974b23159e77a98c32cf4ed696ebf9fc
Yczar <babalolagbogo@gmail.com>
2022-07-20 06:39:18 +0100

    created ui svgs file

---
+++ lib/src/res/svgs/ui_svgs.dart
@@ -0,0 +1,7 @@
+const String _svgDir = 'assets/svgs';
+const String _startSvgDir = '$_svgDir/start';
+const String _cardsSvgDir = '$_svgDir/cards';
+
+///
+const String kArrowRightSVG = '$_startSvgDir/arrow_right.svg';
+const String kMasterCardSVG = '$_cardsSvgDir/mastercard.svg';
